7e671657faab0db8bb917abacc3f6b6f7410729a
princewill <princewillolaiya@gmail.com>
2021-04-07 12:20:34 +0100

    added register login and account generation function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from random import randint
 
 now = datetime.now()
 
@@ -7,15 +8,41 @@ current_time = now.strftime("%H:%M:%S")
 session = True
 current_balance = 0.0
 
+
+def account_number_generation():
+    account = randint(0000000000, 1111111111)
+    return("your account number is " account)
+
+
+def register():
+    first_name = input("enter name: ")
+    last_name = input("enter last name: ")
+    full_name = first_name + last_name
+    account_number = account_number_generation()
+    f = open("storage.txt", "a")
+    f.write("{\n"account_number "\n"full_name "}\n")
+    f.close()
+
+
+def login():
+    match = input("Enter account number: ")
+    file = open('file.txt')
+    for line in file:
+        line.strip().split('/n')
+        if line.startswith(match):
+            print line
+    file.close()
+
+
 while session:
     try:
         print(current_time)
         user_input = input(
-            "1. Withdrawal\n2. Deposit\n3. Complaint\n4. Exit\n..."
+            "1. Register\n2. Login\n3. Withdrawal\n4. Deposit\n5. Complaint\n6. Exit\n..."
         )
         option = float(user_input)
 
-        if option == 1:
+        if option == 3:
             withdrawal = input("How much would you like to withdraw\n..")
             print("Take your cash\n")
             input("Would you like to make another transaction?").lower()
@@ -23,7 +50,7 @@ while session:
                 print("Thank you for banking with us")
                 session = False
 
-        if option == 2:
+        if option == 4:
             withdrawal = input("How much would you like to deposit\n..")
             added_amout = float(withdrawal)
             current_balance = current_balance + added_amout
@@ -33,7 +60,7 @@ while session:
                 print("Thank you for banking with us")
                 session = False
 
-        if option == 3:
+        if option == 5:
             complaint = input("What issue will you like to report?\n..")
             print("Thank you for contacting us")
             input("Would you like to make another transaction?").lower()
@@ -41,7 +68,7 @@ while session:
                 print("Thank you for banking with us")
                 session = False
 
-        if option == 4:
+        if option == 6:
             print("Thank you for banking with us")
             session = False
 
